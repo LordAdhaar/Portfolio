08e97f1542e7cb75cbb4a1511440601009d3e1df
LordAdhaar <jainadhaar2002@gmail.com>
2023-03-12 12:41:16 +0530

    add Logo to Contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,16 @@
 import {BsBoxArrowInUpRight,BsArrowUpRight} from  "react-icons/bs";
 import {FiArrowUpRight} from "react-icons/fi"
+import {AiFillGithub,AiOutlineTwitter,AiFillMail} from "react-icons/ai";
+import {BsLinkedin} from "react-icons/bs";
+import {IoMdMail} from "react-icons/io";
 
 export default function Contact(){
     return(
         <ul className="contact listStyleType">
-            <li style={{height:"100%"}}><a href="https://github.com/LordAdhaar" target="_blank">Github<FiArrowUpRight/></a></li>
-            <li style={{height:"5%"}}><a href="https://www.linkedin.com/in/adhaar-jain-6013121a5" target="_blank">LinkedIn<FiArrowUpRight/></a></li>
-            <li style={{height:"55%"}} ><a href="https://twitter.com/JainAdhaar" target="_blank">Twitter<FiArrowUpRight/></a></li>
-            <li style={{height:"100%"}} ><a href="mailto: adhaar1@hotmail.com" target="_blank">E-Mail<FiArrowUpRight/></a></li>
+            <li style={{height:"100%"}}><a href="https://github.com/LordAdhaar" target="_blank"><AiFillGithub/>Github</a></li>
+            <li style={{height:"5%"}}><a href="https://www.linkedin.com/in/adhaar-jain-6013121a5" target="_blank"><BsLinkedin/>LinkedIn</a></li>
+            <li style={{height:"55%"}} ><a href="https://twitter.com/JainAdhaar" target="_blank"><AiOutlineTwitter/>Twitter</a></li>
+            <li style={{height:"100%"}} ><a href="mailto: adhaar1@hotmail.com" target="_blank"><IoMdMail/>E-Mail</a></li>
         </ul>
     )
 }
