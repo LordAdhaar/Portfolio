9f3b0ec6faedb1489e279ae357f0d147d86f5655
LordAdhaar <jainadhaar2002@gmail.com>
2023-03-12 18:52:34 +0530

    add rotating hovering ginormous pointer

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -7,9 +7,24 @@ import Skill from "./components/Skill";
 import Contact from "./components/Contact"
 
 export default function RouteSwitch(){
+
+    const blob = document.querySelector("div.blob");
+
+    document.body.onpointermove = event => {
+        console.log(event);
+        const {clientX, clientY} = event;
+        
+        blob.animate({
+            left:`${clientX}px`,
+            top:`${clientY}px`
+        },{duration:1500,fill:"forwards"});
+    }
+
     return(
         <HashRouter>
             <Nav />
+            <div className="blob">sheesh</div>
+            <div className="blur"></div>
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="/Projects" element={<Projects/>} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 export default function Home(){
     return(
-        <p className="home">Hi, I'm a front-end developer based in Delhi. I love minimalist and brutalist designs. Oh, and watch Berserk</p>
+        <p className="home">Hi, I'm a front-end developer based in Delhi. I love minimalist and brutalist designs. Oh, and watch Berserk
+        </p>
     )
 }
